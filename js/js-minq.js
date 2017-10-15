465ad9e63ecd5e48604bc1f4068c2bf4ec42af04
Vitaliy <annecery@yandex.ru>
2017-10-15 16:12:14 +0300

    production

---
+++ js/js-minq.js
@@ -0,0 +1,14 @@
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
+
+navMain.classList.remove("main-nav--nojs");
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.toggle("main-nav--opened");
+    navMain.classList.remove("main-nav--closed");
+  } else {
+    navMain.classList.toggle("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
+  }
+});
